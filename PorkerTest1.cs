83cbb843ca293eda1ec06be8ab448c440012dee1
robbin hsu <robbin.hsu@collaborate.tw>
2020-12-23 14:14:45 +0800

    Refactor

--- PorkerTest1.cs
+++ PorkerTest1.cs
@@ -5,30 +5,29 @@ namespace PokerTest
 {
     public class Tests
     {
+        private Dictionary<int, List<int>> _actual;
         private Poker _poker;
 
         [SetUp]
         public void Setup()
         {
             _poker = new Poker();
+            _actual = _poker.Licensing();
         }
 
         [Test]
         public void All_card_in_four_group()
         {
-            var actual = _poker.Licensing();
-            Assert.AreEqual(4, actual.Count);
+            Assert.AreEqual(4, _actual.Count);
         }
 
         [Test]
         public void Every_group_have_thirteen_cards()
         {
-            var actual = _poker.Licensing();
-
-            Assert.AreEqual(13, actual[0].Count);
-            Assert.AreEqual(13, actual[1].Count);
-            Assert.AreEqual(13, actual[2].Count);
-            Assert.AreEqual(13, actual[3].Count);
+            Assert.AreEqual(13, _actual[0].Count);
+            Assert.AreEqual(13, _actual[1].Count);
+            Assert.AreEqual(13, _actual[2].Count);
+            Assert.AreEqual(13, _actual[3].Count);
         }
     }
 }
